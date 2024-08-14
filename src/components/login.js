986bef8be2ab907f47fbe333386a593391011be3
Karan Paigude <paigudekaran2827@gmail.com>
2024-08-14 14:09:19 +0530

    dhruv mulay

---
+++ src/components/login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you would typically send the username and password to your server
+    console.log('Login attempted with username:', username, 'and password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Userme:</label>
+          <input
+            type="text"
+            id="userne"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/components/navBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Nav() {
+    return(
+        <nav>
+            <img src="/images/logo.png" className="nav--logo"alt="" />
+            <div className="navbar-right">
+                {/* <a href="#" className="button1">Login</a>
+                <a href="#" className="button2">Signup</a> */}
+            </div>
+        </nav>
+    )
+}
